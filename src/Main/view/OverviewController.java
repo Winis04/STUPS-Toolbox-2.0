79d998fb8714a3c89d497982fcc53dac14b12554
Isabel Wingen <Isabel.Wingen@hhu.de>
2016-11-30 17:23:01 +0100

    context menu works

--- src/Main/view/OverviewController.java
+++ src/Main/view/OverviewController.java
@@ -1,19 +1,21 @@
 package Main.view;
 
-import AutomatonSimulator.Automaton;
-import Console.CLI;
-import GUIPlugins.TabPlugins.TabPlugin;
 
+
+import AutomatonSimulator.Automaton;
+import GrammarSimulator.Grammar;
 import Main.GUI;
+import com.sun.webkit.ContextMenuItem;
+import javafx.beans.binding.Bindings;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeView;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -29,6 +31,10 @@ public class OverviewController {
     BorderPane contentPane;
     @FXML
     FlowPane simpleFunctionPane;
+
+    ContextMenu dynamicContextMenu=new ContextMenu();
+
+    private HashMap<Class, List<MenuItem>> map = new HashMap<>();
 
 
     private GUI gui;
@@ -48,26 +54,82 @@ public class OverviewController {
     private void initialize() {
 
     }
+
     public void makeTree() {
+        dynamicContextMenu=new ContextMenu();
+
         TreeItem<String> root=new TreeItem<>("Storables");
+
         if(!gui.getCli().store.keySet().isEmpty()) {
             /** top items **/
 
             gui.getCli().store.keySet().stream()
                     .forEach(clazz -> {
-                        List<TreeItem<String>> list= gui.getCli().store.get(clazz).keySet().stream()
-                                .map(key -> new TreeItem<String>(key+""))
-                                .collect(Collectors.toList());
+                        // the name of the storable objects
                         TreeItem<String> top=new TreeItem<String>(clazz.getSimpleName());
+                        // every object of the top-type
+                        List<TreeItem<String>> list= gui.getCli().store.get(clazz).keySet().stream()
+                                .map(key -> new TreeItem<>(key+""))
+                                .collect(Collectors.toList());
+
                         top.getChildren().addAll(list);
                         top.setExpanded(true);
                         root.getChildren().add(top);
                     });
 
+
+            /** context menus **/
+
+            /**
+             * grammar
+             */
+            MenuItem editGrammar=new MenuItem("edit");
+            ArrayList<MenuItem> grammarList=new ArrayList<>();
+            grammarList.add(editGrammar);
+            /**
+             * minimize
+             */
+            MenuItem minimize=new Menu("minimize");
+            ArrayList<MenuItem> autoList=new ArrayList<>();
+            autoList.add(minimize);
+            map.put(Grammar.class,grammarList);
+            map.put(Automaton.class,autoList);
+
+
+
+            treeView.addEventHandler(MouseEvent.MOUSE_RELEASED, e -> {
+                if (e.getButton()==MouseButton.SECONDARY) {
+                    TreeItem selected = treeView.getSelectionModel().getSelectedItem();
+
+                    //item is selected - this prevents fail when clicking on empty space
+                    if (selected!=null) {
+                        //open context menu on current screen position
+                       if(selected.getParent().getValue().equals("Grammar")) {
+                           this.openContextMenu(map,Grammar.class,e.getScreenX(),e.getScreenY());
+                       } else {
+                           this.openContextMenu(map,Automaton.class,e.getScreenX(),e.getScreenY());
+                       }
+                    }
+                } else {
+                    //any other click cause hiding menu
+                    dynamicContextMenu.hide();
+                }
+            });
+
+
             root.setExpanded(true);
             treeView.setRoot(root);
 
+
         }
+    }
+
+    private void openContextMenu(HashMap<Class,List<MenuItem>> map, Class clazz,double x, double y) {
+        dynamicContextMenu.getItems().clear();
+        dynamicContextMenu.getItems().addAll(map.get(clazz));
+
+        //show menu
+        dynamicContextMenu.show(treeView, x, y);
     }
     public void setGui(GUI gui) {
         this.gui = gui;
