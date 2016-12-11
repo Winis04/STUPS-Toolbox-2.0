0ab0d80eb22d533cdbc3a52c76fe3c7dfb771de7
Isabel Wingen <Isabel.Wingen@hhu.de>
2016-12-11 18:17:49 +0100

    made copy generic

--- src/GUIPlugins/SimpleFunctionPlugins/Copy.java
+++ src/GUIPlugins/SimpleFunctionPlugins/Copy.java
@@ -3,6 +3,7 @@ package GUIPlugins.SimpleFunctionPlugins;
 import AutomatonSimulator.Automaton;
 import Console.Storable;
 import GrammarSimulator.Grammar;
+import javafx.scene.control.TreeItem;
 
 /**
  * Created by Isabel on 03.12.2016.
@@ -20,21 +21,13 @@ public class Copy extends SimpleFunctionPlugin {
 
         Storable storable = (Storable) object;
         Storable copy = storable.deep_copy();
-        //TODO automatisation
-        if(gui.getOverviewController().getTreeView().getSelectionModel().getSelectedItem().getParent().getValue().equals("Grammar")) {
-            Grammar grammar = (Grammar) copy;
-            String name = grammar.getName();
-             gui.addToStore(grammar,Grammar.class,name+"_Copy");
-            return copy;
-        } else if (gui.getOverviewController().getTreeView().getSelectionModel().getSelectedItem().getParent().getValue().equals("Automaton")) {
-            Automaton automaton = (Automaton) copy;
-            String name = automaton.getName();
-            automaton.setName(name+"_Copy");
-            gui.addToStore(automaton,Automaton.class,name+"_Copy");
-            return copy;
-        } else {
-            return null;
-        }
+        TreeItem<String> selected = gui.getOverviewController().getTreeView().getSelectionModel().getSelectedItem();
+        String parent = selected.getParent().getValue().toLowerCase();
+        Class parentClass = gui.getCli().lookUpTable.get(parent);
+        String name = copy.getName();
+        gui.addToStore(copy,parentClass,name+"_Copy");
+
+        return copy;
     }
 
 
